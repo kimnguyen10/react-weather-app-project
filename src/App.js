6ecbb05f58a0f2908bbf2b4ae17a6cec844485f2
kim <kimmike19@gmail.com>
2024-05-14 00:13:45 -0400

    Added rel=noreferrer to fix code

--- src/App.js
+++ src/App.js
@@ -6,13 +6,14 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://kimmie.io/" target="_blank">
+        <a href="https://kimmie.io/" target="_blank" rel="noreferrer">
           Kim Nguyen
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/kimnguyen10/react-weather-app-project"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on Github
         </a>
