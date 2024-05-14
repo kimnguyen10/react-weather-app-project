3d26325fd39c8efd01cacbf6502166719ccb80d3
kim <kimmike19@gmail.com>
2024-05-14 00:26:13 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://kimmie.io/" target="_blank" rel="noreferrer">
-          Kim Nguyen
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kimnguyen10/react-weather-app-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://kimmie.io/" target="_blank" rel="noreferrer">
+            Kim Nguyen
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kimnguyen10/react-weather-app-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
