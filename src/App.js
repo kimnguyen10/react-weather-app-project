aebbea046e48790827b5feee41496a00eaa4870f
kim <kimmike19@gmail.com>
2024-05-14 00:10:33 -0400

    Added the Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://kimmie.io/" target="_blank">
+          Kim Nguyen
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kimnguyen10/react-weather-app-project"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
